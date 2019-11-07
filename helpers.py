053af335b40d6e79ccb28824e48f14eebffbfad8
Simon Perriard <simon.perriard@epfl.ch>
2019-11-07 23:00:22 +0000

    Added extract_test_data

--- helpers.py
+++ helpers.py
@@ -21,3 +21,22 @@ def extract_data(filename, num_images):
    
 
     return np.asarray(imgs)
+
+def extract_test_data(filename, num_images):
+    """Extract the images into a 4D tensor [image index, y, x, channels].
+    Values are rescaled from [0, 255] down to [-0.5, 0.5].
+    """
+    imgs = []
+    for i in range(1, num_images+1):
+        folder_id = "test_%d/" % i
+        imageid = "test_%d" % i
+        image_filename = filename + folder_id + imageid + ".png"
+        if os.path.isfile(image_filename):
+            print('Loading ' + image_filename)
+            img = mpimg.imread(image_filename)
+            imgs.append(img)
+        else:
+            print('File ' + image_filename + ' does not exist')
+   
+
+    return np.asarray(imgs)
