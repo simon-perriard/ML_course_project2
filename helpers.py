8625d60642b70a0d6c018e7ed6d97d4fe8f49b7e
root <simon.perriard@epfl.ch>
2019-11-07 14:06:28 +0000

    modified helpers. Treat image as one patch

--- helpers.py
+++ helpers.py
@@ -3,7 +3,6 @@ import os
 import numpy as np
 import matplotlib.image as mpimg
 
-IMG_PATCH_SIZE = 400
 
 def extract_data(filename, num_images):
     """Extract the images into a 4D tensor [image index, y, x, channels].
@@ -19,13 +18,6 @@ def extract_data(filename, num_images):
             imgs.append(img)
         else:
             print('File ' + image_filename + ' does not exist')
+   
 
-    num_images = len(imgs)
-    IMG_WIDTH = imgs[0].shape[0]
-    IMG_HEIGHT = imgs[0].shape[1]
-    N_PATCHES_PER_IMAGE = (IMG_WIDTH/IMG_PATCH_SIZE)*(IMG_HEIGHT/IMG_PATCH_SIZE)
-
-    img_patches = [img_crop(imgs[i], IMG_PATCH_SIZE, IMG_PATCH_SIZE) for i in range(num_images)]
-    data = [img_patches[i][j] for i in range(len(img_patches)) for j in range(len(img_patches[i]))]
-
-    return np.asarray(data)
+    return np.asarray(imgs)
