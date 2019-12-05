9d95815742176def3422559d976fe58ff46c61a9
Hédi Sassi <hedi.sassi96@gmail.com>
2019-12-05 18:15:43 +0000

    removed unnecessary lines in mask_to_submission

--- mask_to_submission.py
+++ mask_to_submission.py
@@ -34,13 +34,3 @@ def masks_to_submission(submission_filename, *image_filenames):
         f.write('id,prediction\n')
         for fn in image_filenames[0:]:
             f.writelines('{}\n'.format(s) for s in mask_to_submission_strings(fn))
-
-
-if __name__ == '__main__':
-    submission_filename = 'dummy_submission.csv'
-    image_filenames = []
-    for i in range(1, 51):
-        image_filename = 'training/groundtruth/satImage_' + '%.3d' % i + '.png'
-        print image_filename
-        image_filenames.append(image_filename)
-    masks_to_submission(submission_filename, *image_filenames)
